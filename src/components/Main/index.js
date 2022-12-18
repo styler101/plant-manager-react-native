d4088a896bf7a71e71489908e2b0f63fab509eb9
Lucas <lucas.santos@mundowap.com.br>
2022-12-18 20:49:45 -0300

    styles: create global styles

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 import { Routes } from "../../routes";
 
 export function Main() {
-  return <Routes />;
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+  return !fontsLoaded ? <ActivityIndicator /> : <Routes />;
 }
---
+++ src/styles/global/index.js
@@ -0,0 +1,24 @@
+export default globalStyles = {
+  colors: {
+    gray: {
+      light: "#D2D3DC",
+      medium: "#9FA3B0",
+      dark: "#777A85",
+    },
+    black: "#0A110A",
+    green: "#39604F",
+  },
+
+  spacing: {
+    sm: 8,
+    md: 12,
+    lg: 16,
+    xl: 24,
+  },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    semi: "Poppins_600SemiBold",
+    bold: "Poppins_700Bold",
+  },
+};
