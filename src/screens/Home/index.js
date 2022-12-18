be2bbe1782c81f60cbb62db0ef44d7934cb9ee49
Lucas <lucas.santos@mundowap.com.br>
2022-12-18 20:35:57 -0300

    feat: create home and details screens

---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text> Home Screen</Text>
+    </View>
+  );
+}
